faa09c69bf83cab542768f627327038dbba51e85
akutowicz_20.02.26 <ignacjo122@gmail.com>
2026-04-10 12:30:51 +0200

    Dodanie projektu django

---
+++ pso/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Car(models.Model):
+    title = models.CharField(max_length=200)
+    brand = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    year = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return  self.title
