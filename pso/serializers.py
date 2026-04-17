1471cb7e58d0f419553c39b75a748f81f7f0e8d2
akutowicz_20.02.26 <ignacjo122@gmail.com>
2026-04-17 12:47:06 +0200

    Dodalem rejestracje uzytkownika , w pliku readme w main jest hierarchia jak odpalic

--- pso/serializers.py
+++ pso/serializers.py
@@ -1,7 +1,23 @@
 from rest_framework import serializers
 from .models import Car
+from django.contrib.auth.models import User
 
 class CarSerializer(serializers.ModelSerializer):
     class Meta:
         model = Car
         fields = ['id', 'brand', 'model', 'year', 'price', 'mileage', 'description', 'created_at']
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username = validated_data['username'],
+            password = validated_data['password']
+        )
+        return user
--- pso/views.py
+++ pso/views.py
@@ -2,6 +2,16 @@ from rest_framework import viewsets
 from .models import Car
 from .serializers import  CarSerializer
 
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from .serializers import RegisterSerializer
+from django.contrib.auth.models import User
+
 class CarViewSet(viewsets.ModelViewSet):
     queryset = Car.objects.all()
     serializer_class = CarSerializer
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = [AllowAny]
+    serializer_class = RegisterSerializer
